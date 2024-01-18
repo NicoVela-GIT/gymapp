e851a8da4e856bba6788e74dd032530ae2fdce7b
NicoVela-GIT <154269707+NicoVela-GIT@users.noreply.github.com>
2024-01-18 17:47:06 -0500

    style edit

--- src/compnents/Hero.jsx
+++ src/compnents/Hero.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <div className='min-h-screen flex flex-col gap-10 items-center justify-center'>
+        <p>IT'S TIME TO GET</p>
+        <h1 className='uppercase font-bold text-5xl sm:text-6xl md:text-7xl lg:text-8xl'>Your<span>GymBag</span></h1>
+        <p>Welcome to <span>GymBagBuddy,</span> your new gym bag companion, your gym bro, and the ultimate acountability partner. <span>GymBagBuddy</span> was designed to keep you in check when you're at the gym and to take away the task of thinking through your workoouts. So grab your gym bag and hit some weights with us</p>
+    </div>
   )
 }
